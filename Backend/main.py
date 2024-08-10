fec1315c4f41779cc0de73293b2dcb066f7229a1
Roni Milstein <128286911+RoniMil@users.noreply.github.com>
2024-08-10 16:32:43 +0300

    implement basic submit function logic

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,5 @@
 import os
+from bson import ObjectId
 from dotenv import find_dotenv, load_dotenv
 from pydantic import BaseModel
 from pymongo import MongoClient
@@ -19,10 +20,13 @@ jdoodle_client_secret = os.environ.get("JDOODLE_CLIENT_SECRET")
 connection_str = f"mongodb+srv://{mongodb_user}:{mongodb_pwd}@develocoop.vjutg.mongodb.net/?retryWrites=true&w=majority&appName=DeveloCoop"
 client = MongoClient(connection_str, server_api=ServerApi('1'))
 
-dbs = client.list_database_names()
-
 questions = client.DeveloCoop.Questions
 
+
+# class for submit answer post request framework
+class Submission(BaseModel):
+    question_id: str
+    user_answer: str
 
 @app.get("/questions/")
 def get_question():
@@ -30,20 +34,22 @@ def get_question():
     question = questions.aggregate([{"$sample": {"size": 1}}]).next()
     return question["Question Name"]
 
-@app.get("/questions/{question_id}")
-def get_results(question_id, user_answer : str = Query(...)):
+@app.post("/questions/submit")
+def get_results(submission : Submission):
+    test = client.DeveloCoop.test
     # from the user POSSIBLE TRANSFORMATION INTO \n\t FORMAT NEEDED
-    question = "def fuc(n):\n\tif n == 1 or n == 0:\n\t\treturn 1\n\treturn n * fuc(n-1)\n"
+    user_answer = submission.user_answer
+    # question = "def fuc(n):\n\tif n == 1 or n == 0:\n\t\treturn 1\n\treturn n * fuc(n-1)\n"
     # predefined from the excel sheet given a question id
-    tests = "import math\n\ttests = [i for i in range(0, 10)]\n\tfor t in tests:\n\t\tassert fuc(t) == math.factorial(t)\n"
-
+    #tests = "import math\n\ttests = [i for i in range(0, 10)]\n\tfor t in tests:\n\t\tassert fuc(t) == math.factorial(t)\n"
+    tests = test.find_one({"_id" : ObjectId(submission.question_id)})["Tests"]
     # specific tests for the question id
-    test = f"if __name == '__main__':\n\t{tests}"
+    test = f"\nif __name__ == '__main__':\n\t{tests}"
 
     execution_data = {
         'clientId': jdoodle_client_id,
         'clientSecret': jdoodle_client_secret,
-        'script': question + test,
+        'script': user_answer + test,
         'language': 'python3',
         'versionIndex': '0'
     }
@@ -54,16 +60,9 @@ def get_results(question_id, user_answer : str = Query(...)):
 
     # !!!handle timeout case!!!
     # Output the result
-    print(response.json())
+    return response.json()
 
 # def get_followup
-
-class Question(BaseModel):
-    name: str
-    description: str | None = None
-    examples: str | None = None
-    # declaration: float
-    # tests: float | None = None
 
 
 
