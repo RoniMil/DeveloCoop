f6e019833ad7e8caeb67e1ea637371d7f50a4c9e
Roni Milstein <128286911+RoniMil@users.noreply.github.com>
2024-08-12 15:19:54 +0300

    revise data format, implement testing framework using JDoodle queries

--- Backend/main.py
+++ Backend/main.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 import requests
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 import uvicorn
 
 load_dotenv(find_dotenv())
@@ -20,7 +20,16 @@ jdoodle_client_secret = os.environ.get("JDOODLE_CLIENT_SECRET")
 connection_str = f"mongodb+srv://{mongodb_user}:{mongodb_pwd}@develocoop.vjutg.mongodb.net/?retryWrites=true&w=majority&appName=DeveloCoop"
 client = MongoClient(connection_str, server_api=ServerApi('1'))
 
-questions = client.DeveloCoop.Questions
+questions_db = client.DeveloCoop.Questions
+test_db = client.DeveloCoop.test
+
+test_hardcoded_str = """wrong_answers = [(test, user_solution(*test), solution(*test)) for test in test_cases if user_solution(*test) != solution(*test)]
+if not wrong_answers:
+    print('Passed all tests!')
+else:
+    print('Failed on the following tests:')
+    for test_case, your_answer, answer in wrong_answers:
+        print("On input: " + list(test_case) + ", your result: " + str(your_answer) + ", expected answer: " + str(answer))"""
 
 
 # class for submit answer post request framework
@@ -31,25 +40,31 @@ class Submission(BaseModel):
 @app.get("/questions/")
 def get_question():
     print("run get_question")
-    question = questions.aggregate([{"$sample": {"size": 1}}]).next()
+    # !!!change DB to questions when actual DB exists!!!
+    question = test_db.aggregate([{"$sample": {"size": 1}}]).next()
     return question["Question Name"]
 
 @app.post("/questions/submit")
 def get_results(submission : Submission):
-    test = client.DeveloCoop.test
     # from the user POSSIBLE TRANSFORMATION INTO \n\t FORMAT NEEDED
     user_answer = submission.user_answer
+    test_db = client.DeveloCoop.test
     # question = "def fuc(n):\n\tif n == 1 or n == 0:\n\t\treturn 1\n\treturn n * fuc(n-1)\n"
     # predefined from the excel sheet given a question id
     #tests = "import math\n\ttests = [i for i in range(0, 10)]\n\tfor t in tests:\n\t\tassert fuc(t) == math.factorial(t)\n"
-    tests = test.find_one({"_id" : ObjectId(submission.question_id)})["Tests"]
+    db_entry = test_db.find_one({"_id" : ObjectId(submission.question_id)})
+    tests = db_entry["Question Tests"]
+    answer = db_entry["Question Solution"]
+
     # specific tests for the question id
-    test = f"\nif __name__ == '__main__':\n\t{tests}"
+    #test_hardcoded_str = "wrong_answers = [(test,\tuser_solution(*test),\tsolution(*test))\tfor test in test_cases\tif user_solution(*test) != solution(*test)]\nif not wrong_answers:\n\tprint('Passed all tests!')\nelse:\n\tprint('Failed on the following tests:')\n\tfor test_case,\tyour_answer,\tanswer in wrong_answers:\n\t\tprint(f'On input: {test_case},\tyour result: {your_answer},\texpected answer: {answer}')"
+
+    test_str = f"{tests}\n{test_hardcoded_str}"
 
     execution_data = {
         'clientId': jdoodle_client_id,
         'clientSecret': jdoodle_client_secret,
-        'script': user_answer + test,
+        'script': f"{answer}\n{user_answer}\n{test_str}",
         'language': 'python3',
         'versionIndex': '0'
     }
